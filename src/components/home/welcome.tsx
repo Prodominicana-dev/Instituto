ce64790ac711735653a68ba3d52c29b004fbc5ea
eliampimentel <139479508+eliampimentel@users.noreply.github.com>
2024-02-12 12:50:08 -0400

    color card

--- src/components/home/welcome.tsx
+++ src/components/home/welcome.tsx
@@ -6,13 +6,13 @@ import {
   AcademicCapIcon,
   HomeModernIcon,
   UserIcon,
-} from "@heroicons/react/16/solid";
+} from "@heroicons/react/24/solid";
 
 export default function WelcomeSection() {
   return (
     <section
       id="welcome"
-      className="w-full h-[60vh] bg-gradient-to-br from-blue-800 to-blue-900 flex flex-col gap-8 justify-center items-center"
+      className="w-full h-[60vh] bg-gradient-to-b from-blue-600 to-blue-950 flex flex-col gap-8 justify-center items-center"
     >
       <h1 className="font-light text-4xl">
         Bienvenido al Instituto ProDominicana
@@ -23,25 +23,25 @@ export default function WelcomeSection() {
       </p>
       <div className="grid grid-cols-4 gap-5 justify-center items-center">
         <WelcomeCard
-          color="bg-cyan-600"
+          color="bg-lightBlue-500"
           title="GRADUACIONES"
           description="90%"
           icon={<AcademicCapIcon className="size-12 fill-white/60" />}
         />
         <WelcomeCard
-          color="bg-green-600"
+          color="bg-teal-600"
           title="POSTGRADOS"
           description="20+"
           icon={<BriefcaseIcon className="size-12 fill-white/60" />}
         />
         <WelcomeCard
-          color="bg-lime-800"
+          color="bg-lime-500"
           title="AULAS"
           description="60+"
           icon={<HomeModernIcon className="size-12 fill-white/60" />}
         />
         <WelcomeCard
-          color="bg-purple-600"
+          color="bg-fuchsia-600"
           title="ESTUDIANTES"
           description="1234"
           icon={<UserIcon className="size-12 fill-white/60" />}
