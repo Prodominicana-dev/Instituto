7418f6d76ddb13a89e09940a69c360da67870c0c
eliampimentel <139479508+eliampimentel@users.noreply.github.com>
2024-02-12 12:35:02 -0400

    fix main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,5 @@ import WelcomeSection from "@/components/home/welcome";
 import Image from "next/image";
 
 export default function Home() {
-<<<<<<< HEAD
   return <WelcomeSection />;
-=======
-  return <main className="bg-white h-screen"></main>;
->>>>>>> 7f67cf6e6be6533dd13dfd197601e154543ad6ee
 }
