9895ef8a5a5a97e20b9d26fcad49e91d0e8f3c9c
Randy Frias <randyfrias@prodominicana.gob.do>
2024-02-13 13:10:03 -0400

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg"></link>
+      </head>
       <body className={inter.className}>
         <Navbar />
         <NavBarMobile />
